1c4d1fca1b9513ff55c217a5e5542581b6899e22
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2021-01-11 14:18:56 -0500

    added nth function

--- Radix.java
+++ Radix.java
@@ -1,3 +1,14 @@
 public class Radix {
 
+  public static int nth(int n, int col) {
+    int nth;
+    col = col + 1;
+    nth = n / (int)Math.pow(10, col - 1) % 10;
+    return nth;
+  }
+
+  public static int length(int n) {
+    
+  }
+
 }
