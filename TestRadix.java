899bcf4107ee600daea6787c3db69f3c346724e7
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2021-01-11 16:58:15 -0500

    updated tester

--- TestRadix.java
+++ TestRadix.java
@@ -12,35 +12,35 @@ public class TestRadix {
     System.out.println(Radix.length(-10));
     System.out.println(Radix.length(5112));
 
-    MyLinkedList o = new MyLinkedList();
+    SortableLinkedList o = new SortableLinkedList();
 
-    MyLinkedList n0 = new MyLinkedList();
-    MyLinkedList n1 = new MyLinkedList();
-    MyLinkedList n2 = new MyLinkedList();
-    MyLinkedList n3 = new MyLinkedList();
-    MyLinkedList n4 = new MyLinkedList();
-    MyLinkedList n5 = new MyLinkedList();
-    MyLinkedList n6 = new MyLinkedList();
-    MyLinkedList n7 = new MyLinkedList();
-    MyLinkedList n8 = new MyLinkedList();
-    MyLinkedList n9 = new MyLinkedList();
+    SortableLinkedList n0 = new SortableLinkedList();
+    SortableLinkedList n1 = new SortableLinkedList();
+    SortableLinkedList n2 = new SortableLinkedList();
+    SortableLinkedList n3 = new SortableLinkedList();
+    SortableLinkedList n4 = new SortableLinkedList();
+    SortableLinkedList n5 = new SortableLinkedList();
+    SortableLinkedList n6 = new SortableLinkedList();
+    SortableLinkedList n7 = new SortableLinkedList();
+    SortableLinkedList n8 = new SortableLinkedList();
+    SortableLinkedList n9 = new SortableLinkedList();
 
     for (int i = 0; i < 10; i++) {
-      o.add("" + i);
+      o.add(i);
     }
 
-    n0.add("10");
-    n1.add("11");
-    n2.add("12");
-    n3.add("13");
-    n4.add("14");
-    n5.add("15");
-    n6.add("16");
-    n7.add("17");
-    n8.add("18");
-    n9.add("19");
+    n0.add(10);
+    n1.add(11);
+    n2.add(12);
+    n3.add(13);
+    n4.add(14);
+    n5.add(15);
+    n6.add(16);
+    n7.add(17);
+    n8.add(18);
+    n9.add(19);
 
-    MyLinkedList[] b = {n0, n1, n2, n3, n4, n5, n6, n7, n8, n9};
+    SortableLinkedList[] b = {n0, n1, n2, n3, n4, n5, n6, n7, n8, n9};
 
     System.out.println(o.toString());
     Radix.merge(o,b);
