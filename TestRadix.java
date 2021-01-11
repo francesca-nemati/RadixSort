e7ee2d02fd7e65e9c0ff3d8dfdbeb247970dd1e5
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2021-01-11 14:27:19 -0500

    basic tester

---
+++ TestRadix.java
@@ -0,0 +1,15 @@
+public class TestRadix {
+
+  public static void main(String[] args) {
+    System.out.println(Radix.nth(123, 1));
+    System.out.println(Radix.nth(-123, 1));
+    System.out.println(Radix.nth(123, 2));
+    System.out.println(Radix.nth(-123, 2));
+    System.out.println();
+    System.out.println(Radix.length(0));
+    System.out.println(Radix.length(15));
+    System.out.println(Radix.length(-10));
+    System.out.println(Radix.length(5112));
+  }
+
+}
