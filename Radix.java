6aef4ea4c89155d6d9d959da985cfa1bc38b436e
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2021-01-11 14:27:07 -0500

    tested and fixed nth and length

--- Radix.java
+++ Radix.java
@@ -3,12 +3,15 @@ public class Radix {
   public static int nth(int n, int col) {
     int nth;
     col = col + 1;
+    n = Math.abs(n);
     nth = n / (int)Math.pow(10, col - 1) % 10;
     return nth;
   }
 
   public static int length(int n) {
     int len;
+    n = Math.abs(n);
+    if (n == 0) return 1;
     len = (int)(Math.log10(n) + 1);
     return len;
   }
